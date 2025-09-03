1a34a80812aef0ba8f783053f40fc24f75f9354d
González Pau (F4E-Ext) <Pau.Gonzalez@ext.f4e.europa.eu>
2025-09-03 11:31:55 +0200

    remove lines to handle exceptions with eKMP duplicates

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,7 @@
 import os
 # set environment variables to fix problem with easyocr
 # OMP: Error #15: Initializing libiomp5md.dll, but found libiomp5md.dll already initialized.
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+# os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 from babel.numbers import get_currency_symbol, get_territory_currencies
 from copy import deepcopy
 from datetime import datetime
